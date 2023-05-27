8c96932cfd2ae24b83ec9fcbb2d02d3e96650f91
kashifkhanniazi <54848900+kashifkhanniazi@users.noreply.github.com>
2023-05-27 10:28:19 +0500

    add attribute routing

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        [Route("movies/random")]
+        public IActionResult Random()
+        {
+            Movie movie= new Movie() {Name="Shrik!"};
+            return View(movie);
+        }
+        [Route("movies/Index/{pageIndex:regex(\\d{{4}})}/{SortBy}")]
+        public IActionResult Index(int? PageIndex, string SortBy) {
+            if(!PageIndex.HasValue)
+            {
+                PageIndex = 1;
+            }
+            if (string.IsNullOrWhiteSpace(SortBy))
+            {
+                SortBy = "Name";
+            }
+            return Content(string.Format("PageIndex={0} SortBy={1}",PageIndex,SortBy));
+        }
+    }
+}
