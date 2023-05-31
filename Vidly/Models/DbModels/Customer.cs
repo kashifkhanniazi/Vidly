a56972aea5bc1c164e751bfbef24eb92039525a2
kashifkhanniazi <54848900+kashifkhanniazi@users.noreply.github.com>
2023-05-31 11:06:32 +0500

    add dataanotation IN customer table for learning

--- Vidly/Models/DbModels/Customer.cs
+++ Vidly/Models/DbModels/Customer.cs
@@ -7,6 +7,7 @@ namespace Vidly.Models.DbModels
         [Key]
         public int Id { get; set; }
         [Required]
+        [StringLength(250)]
         public string Name { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
